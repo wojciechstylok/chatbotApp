21f70a34225a220d2f4515bb80921097d082d754
wojciech.stylok <wojciech.stylok@externals.hilti.com>
2024-01-22 12:07:57 +0100

    added chat-messages scrolling

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { QuestionAndAnswers } from './models/QuestionAndAnswers';
 import { Message } from './models/Message';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-chat',
@@ -16,7 +16,11 @@ import {MatIconModule} from '@angular/material/icon';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements AfterViewChecked {
+  @ViewChild('chatMessages') private chatMessagesRef: ElementRef | undefined;
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
   messages: Message[] = [];
   userName: string = 'Gość';
   userInput: string = '';
@@ -74,5 +78,12 @@ export class ChatComponent {
       answer.content = 'Nie zrozumiałem co masz na myśli :( Czy możesz powtórzyć?';
     }
     this.messages.push(answer);
+  }
+
+  private scrollToBottom(): void {
+    if (this.chatMessagesRef && this.chatMessagesRef.nativeElement) {
+      const chatMessagesElement: HTMLElement = this.chatMessagesRef.nativeElement;
+      chatMessagesElement.scrollTop = chatMessagesElement.scrollHeight;
+    }
   }
 }
